e2df5e64eea6d24bfb830d0c85bf46c410f3763f
alexxyu <yu.alexanderx@gmail.com>
2020-02-08 17:14:02 -0800

    Fixed anyFalse

--- hw3/hw3/linear.cpp
+++ hw3/hw3/linear.cpp
@@ -10,8 +10,8 @@ bool somePredicate(double x);
 // least one of the array elements; return false otherwise.
 bool anyFalse(const double a[], int n)
 {
-    if(n<=0) return true;
-    return somePredicate(a[0]) || anyFalse(a+1, n-1);
+    if(n<=0) return false;
+    return !somePredicate(a[0]) || anyFalse(a+1, n-1);
 }
 
 // Return the number of elements in the array for which the
@@ -85,10 +85,48 @@ bool somePredicate(double x)
 int main()
 {
     double a[5] = { 1, 1, 5, 6, -3 };
+    assert(anyFalse(a, 5));
     assert(firstTrue(a, 4) == -1);
+    assert(firstTrue(a, 5) == 4);
+    assert(countTrue(a, 5) == 1);
+    assert(countTrue(a, 4) == 0);
+    assert(positionOfSmallest(a, 5) == 4);
     
     double a1[7] = { 10, 50, 40, 20, 50, 40, 30 };
+    {
+        double a2[3] = { 50, 30, 20 };
+        assert(!contains(a1, 7, a2, 3));
+    }
+    {
+        double a2[3] = { 50, 40, 40 };
+        assert(contains(a1, 7, a2, 3));
+    }
+    {
+        double a2[3] = { 50, 20, 30 };
+        assert(contains(a1, 7, a2, 3));
+        assert(contains(a1, 7, a2, 0));
+    }
+    
     double a2[3] = { 10, 20, 20 };
     assert(!contains(a1, 7, a2, 3));
+    assert(anyFalse(a1, 7));
+    assert(firstTrue(a1, 7) == -1);
+    assert(positionOfSmallest(a1, 7) == 0);
+    
+    double b[3] = { -2, -2, 5 };
+    assert(anyFalse(b, 3));
+    assert(!anyFalse(b, 2));
+    assert(!anyFalse(b, 0));
+    assert(countTrue(b, 3) == 2);
+    assert(countTrue(b, 0) == 0);
+    assert(firstTrue(b, 3) == 0);
+    assert(positionOfSmallest(b, 3) == 0);
+    
+    double c[7] = { 1, 8, 3, -1, -5, -5, 10 };
+    assert(firstTrue(c, 7) == 3);
+    assert(positionOfSmallest(c, 7) == 4);
+    assert(positionOfSmallest(c, 4) == 3);
+    assert(positionOfSmallest(c, 0) == -1);
+    
     cout << "Passed all tests" << endl;
 }
