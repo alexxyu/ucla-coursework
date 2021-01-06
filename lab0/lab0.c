7c3523e9961bedee595da54da0a8dc875d07475e
Alex Yu <yu.alexanderx@gmail.com>
2021-01-05 17:25:34 -0800

    Update file redirection

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -68,60 +68,53 @@ int main(int argc, char *argv[]) {
 
     // printf("fin: %s\tfout: %s\tsegflag: %d\tcatchflag: %d\n", fin, fout, segflag, catchflag);
 
-    if(catchflag) signal(SIGSEGV, handleSegFault);
-    if(segflag) forceSegFault();
-
-    int BUFF_SIZE = 512;
-
-    char buffer[BUFF_SIZE];
-    size_t size_read;
     if(fin) {
-        // Read from input file
+        // Handle input file redirection
         int ifd_rd = open(fin, O_RDONLY);
 
         if(ifd_rd >= 0) {
             close(0);
-            size_read = read(ifd_rd, buffer, BUFF_SIZE);
+            dup(ifd_rd);
             close(ifd_rd);
         } else {
-            // Cannot read from given input file
+            // Cannot open given input file
             fprintf(stderr, "Unable to open input file %s: %s\n", fin, strerror(errno));
             _exit(2);
         }
-    } else {
-        // Read from standard input
-        size_read = read(0, buffer, BUFF_SIZE);
-        close(0);
-    }
+    } 
 
     if(fout) {
-        // Write to output file
-        int ifd_wr = open(fout, O_WRONLY);
+        // Handle output file redirection
+        int ifd_wr = creat(fout, 0666);
 
         if(ifd_wr >= 0) {
-            // Able to write to given output file
             close(1);
-            write(ifd_wr, buffer, size_read);
+            dup(ifd_wr);
             close(ifd_wr);
         } else {
-            // Attempt to create new output file
-            ifd_wr = creat(fout, 0666);
-
-            if(ifd_wr >= 0) {
-                close(1);
-                write(ifd_wr, buffer, size_read);
-                close(ifd_wr);
-            } else {
-                // Cannot create given output file
-                fprintf(stderr, "Unable to create output file %s: %s\n", fout, strerror(errno));
-                _exit(3);
-            }
+            // Cannot create given output file
+            fprintf(stderr, "Unable to create output file %s: %s\n", fout, strerror(errno));
+            _exit(3);
         }
-    } else {
-        // Write to standard output
-        write(1, buffer, size_read);
-        close(1);
     }
+
+    if(catchflag) 
+        signal(SIGSEGV, handleSegFault);
+
+    if(segflag) 
+        forceSegFault();
+
+    int BUFF_SIZE = 512;
+    char buffer[BUFF_SIZE];
+    size_t size_read;
+    
+    // Read from input
+    size_read = read(0, buffer, BUFF_SIZE);
+    close(0);
+
+    // Write to output
+    write(1, buffer, size_read);
+    close(1);
 
     _exit(0);
 }
