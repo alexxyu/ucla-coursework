8d4315f2a10de6990ab84732d9ced80e647b2748
Alex Yu <yu.alexanderx@gmail.com>
2021-01-29 21:47:25 -0800

    Implement synchronization (mutex, spin, atomic)

--- lab2a/lab2_add.c
+++ lab2a/lab2_add.c
@@ -15,13 +15,22 @@ ID: 105295708
 #define DEFAULT_THREADS 1
 #define DEFAULT_ITERS 1
 
+#define NO_SYNC 0
+#define M_SYNC 1
+#define S_SYNC 2
+#define C_SYNC 3
+
 long long counter;
 long n_threads, n_iters;
 
-int opt_yield;
+char sync_char;
+int opt_yield, opt_sync;
+
+long spinlock;
+pthread_mutex_t mutex;
 
 void print_usage_and_exit(char* exec) {
-    fprintf(stderr, "Usage: %s --port=PORTNO [--log=FILENAME] [--compress]\n", exec);
+    fprintf(stderr, "Usage: %s [-iterations=<ITERS>] [--threads=<THREADS>] [--yield] [--sync={m|s|c}}]\n", exec);
     exit(1);
 }
 
@@ -29,8 +38,12 @@ void set_test_name(char* name, int size) {
     if(opt_yield) {
         snprintf(name, size, "%s-yield", name);
     } 
-    
-    snprintf(name, size, "%s-none", name);
+
+    if(opt_sync != NO_SYNC) {
+        snprintf(name, size, "%s-%c", name, sync_char);
+    } else {
+        snprintf(name, size, "%s-none", name);
+    }
 }
 
 void add(long long *pointer, long long value) {
@@ -40,12 +53,57 @@ void add(long long *pointer, long long value) {
     *pointer = sum;
 }
 
+void atomic_add(long long *pointer, long long value) {
+    long long prev, sum;
+    do {
+        prev = *pointer;
+        sum = prev + value;
+        if (opt_yield)
+                sched_yield();
+    } while(!__sync_bool_compare_and_swap(pointer, prev, sum));
+}
+
 void *run() {
     for(long i=0; i<n_iters; i++) {
-        add(&counter, 1);
+        switch(opt_sync) {
+            case NO_SYNC:
+                add(&counter, 1);
+                break;
+            case M_SYNC:
+                pthread_mutex_lock(&mutex);
+                add(&counter, 1);
+                pthread_mutex_unlock(&mutex);
+                break;
+            case S_SYNC:
+                while(__sync_lock_test_and_set(&spinlock, 1) == 1);
+                add(&counter, 1);
+                __sync_lock_release(&spinlock);
+                break;
+            case C_SYNC:
+                atomic_add(&counter, 1);
+                break;
+        }
     }
+
     for(long i=0; i<n_iters; i++) {
-        add(&counter, -1);
+        switch(opt_sync) {
+            case NO_SYNC:
+                add(&counter, -1);
+                break;
+            case M_SYNC:
+                pthread_mutex_lock(&mutex);
+                add(&counter, -1);
+                pthread_mutex_unlock(&mutex);
+                break;
+            case S_SYNC:
+                while(__sync_lock_test_and_set(&spinlock, 1) == 1);
+                add(&counter, -1);
+                __sync_lock_release(&spinlock);
+                break;
+            case C_SYNC:
+                atomic_add(&counter, -1);
+                break;
+        }
     }
     return NULL;
 }
@@ -56,6 +114,7 @@ int main(int argc, char *argv[]) {
         {"threads", required_argument, 0, 't'},
         {"iterations", required_argument, 0, 'i'},
         {"yield", no_argument, 0, 'y'},
+        {"sync", required_argument, 0, 's'},
         {0, 0, 0, 0}
     };
     
@@ -63,9 +122,11 @@ int main(int argc, char *argv[]) {
     n_threads = DEFAULT_THREADS;
     n_iters = DEFAULT_ITERS;
     opt_yield = 0;
+    opt_sync = NO_SYNC;
 
     // Process options and arguments
     int c, opt_index;
+    char* sync_str = NULL;
     while( (c = getopt_long(argc, argv, "", long_options, &opt_index)) != -1 ) {
         switch(c) {
             case 't':
@@ -76,6 +137,9 @@ int main(int argc, char *argv[]) {
                 break;
             case 'y':
                 opt_yield = 1;
+                break;
+            case 's':
+                sync_str = optarg;
                 break;
             default:
                 // Handle unrecognized argument
@@ -94,6 +158,29 @@ int main(int argc, char *argv[]) {
     if(n_iters <= 0) {
         fprintf(stderr, "Please enter a valid number of iterations\n");
         print_usage_and_exit(argv[0]);
+    }
+    if(sync_str) {
+        if(strlen(sync_str) > 1) {
+            fprintf(stderr, "Please provide a valid synchronization option\n");
+            print_usage_and_exit(argv[0]);
+        }
+
+        switch((sync_char = sync_str[0])) {
+            case 'm':
+                opt_sync = M_SYNC;
+                pthread_mutex_init(&mutex, NULL);
+                break;
+            case 's':
+                opt_sync = S_SYNC;
+                spinlock = 0;
+                break;
+            case 'c':
+                opt_sync = C_SYNC;
+                break;
+            default:
+                fprintf(stderr, "Please provide a valid synchronization option\n");
+                print_usage_and_exit(argv[0]);
+        }
     }
 
     counter = 0;
@@ -126,6 +213,9 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Error getting clock time: %s\n", strerror(errno));
         exit(1);
     }
+
+    // Destory mutex lock as necessary
+    if(opt_sync == M_SYNC) pthread_mutex_destroy(&mutex);
 
     // Print out CSV record of results
     long n_operations = n_threads * n_iters * 2;
