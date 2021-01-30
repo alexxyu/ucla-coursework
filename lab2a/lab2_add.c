d897fe5abd1c8ee323d8a351f3f09ef5d829b722
Alex Yu <yu.alexanderx@gmail.com>
2021-01-30 12:48:34 -0800

    Change C&S function

--- lab2a/lab2_add.c
+++ lab2a/lab2_add.c
@@ -62,7 +62,7 @@ void atomic_add(long long *pointer, long long value) {
         sum = prev + value;
         if (opt_yield)
                 sched_yield();
-    } while(!__sync_bool_compare_and_swap(pointer, prev, sum));
+    } while(__sync_val_compare_and_swap(pointer, prev, sum) != prev);
 }
 
 void *run() {
