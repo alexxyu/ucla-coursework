ae33483df55a62280161232442f59a979f143b12
Alex Yu <yu.alexanderx@gmail.com>
2021-04-01 19:08:16 -0700

    Add faster BWT matching algorithm
    Faster BWT matching algorithm replaces use of last-to-first mapping with count arrays

--- chapter-9/genome_compression.py
+++ chapter-9/genome_compression.py
@@ -1,5 +1,8 @@
 import time
 
+"""
+Compress text using Burrows-Wheeler Transform (BWT)
+"""
 def compress(file):
     with open(file, 'r') as f:
         text = f.read().splitlines()[0]
@@ -17,6 +20,9 @@ def compress(file):
             BWT += s[-1]
         print(BWT)
 
+"""
+Decompress text using Burrows-Wheeler Transform (BWT)
+"""
 def decompress(file):
     with open(file, 'r') as f:
         transform = f.read().splitlines()[0]
@@ -44,6 +50,9 @@ def decompress(file):
         char_to_find += '$'
         print(text)
 
+"""
+Match text in BWT form to list of patterns and get counts
+"""
 def bwt_match(file):
     with open(file, 'r')  as f:
         bwt, patterns = f.read().splitlines()
@@ -97,4 +106,51 @@ def bwt_match(file):
 
         print(' '.join(occurences))
 
-bwt_match("genome_compression.txt")
+"""
+better_bwt_match is ~ 4x faster than bwt_match by substituting the last-to-first mapping with count arrays
+"""
+def better_bwt_match(file):
+    with open(file, 'r')  as f:
+        bwt, patterns = f.read().splitlines()
+        patterns = patterns.split()
+
+        last_column = [c for c in bwt]
+        first_column = sorted(last_column)
+
+        # Calculate counts for each character up to each index in last_column
+        counts = {c: [0] for c in list(set(last_column))}
+        for i in range(len(last_column)):
+            c = last_column[i]
+            for k in counts.keys():
+                if k == c:
+                    counts[k].append(counts[k][-1]+1)
+                else:
+                    counts[k].append(counts[k][-1])
+
+        # Find first occurence of each character in first_column
+        first_occurrence = dict()
+        for c in counts.keys():
+            first_occurrence[c] = [i for i, k in enumerate(first_column) if k == c][0]
+
+        occurences = []
+        for pattern in patterns:
+            top = 0
+            bottom = len(last_column) - 1
+            while top <= bottom:
+                if len(pattern) > 0:
+                    symbol = pattern[-1]
+                    pattern = pattern[:-1]
+
+                    if symbol in last_column[top:bottom+1]:
+                        top = first_occurrence[symbol] + counts[symbol][top]
+                        bottom = first_occurrence[symbol] + counts[symbol][bottom+1] - 1
+                    else:
+                        occurences.append("0")
+                        break
+                else:
+                    occurences.append(str(bottom - top + 1))
+                    break
+
+        print(' '.join(occurences))
+
+better_bwt_match("genome_compression.txt")
