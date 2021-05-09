5e3916e1093aef38722db19aac5413771eb1a756
Alex Yu <yu.alexanderx@gmail.com>
2021-05-08 20:28:06 -0700

    Fitting alignment + overlap alignment

--- chapter-5/alignment.py
+++ chapter-5/alignment.py
@@ -141,6 +141,12 @@ def local_alignment(file):
 
     return *backtracker(backtrack, str1, str2, x, y), dp[x][y]
 
+"""
+Edit Distance Problem: Find the edit distance between two strings.
+
+Input: Two strings.
+Output: The edit distance between these strings.
+"""
 def edit_distance(file):
     with open(file, 'r') as f:
         str1, str2 = f.read().splitlines()
@@ -159,7 +165,144 @@ def edit_distance(file):
 
     return dp[n][m]
 
-s1, s2, score = local_alignment('alignment.txt')
+"""
+Code Challenge: Solve the Fitting Alignment Problem.
+
+Input: Two nucleotide strings v and w, where v has length at most 1000 and w has length at most 100
+Output: A highest-scoring fitting alignment between v and w. Use the simple scoring method in which 
+    matches count +1 and both the mismatch and indel penalties are 1.
+"""
+def fitting_alignment(file):
+    INDEL_PENALTY = 1
+
+    def backtracker(backtrack, s1, s2, n, m):
+        s1_aligned, s2_aligned = "", ""
+        i, j = n, m
+        while i != 0 or j != 0:
+            if backtrack[i][j] == 's':
+                break
+            elif backtrack[i][j] == 'd':
+                s1_aligned = s1[i-1] + s1_aligned
+                s2_aligned = '-' + s2_aligned
+                i -= 1
+            elif backtrack[i][j] == 'r':
+                s1_aligned = '-' + s1_aligned
+                s2_aligned = s2[j-1] + s2_aligned
+                j -= 1
+            else:
+                s1_aligned = s1[i-1] + s1_aligned
+                s2_aligned = s2[j-1] + s2_aligned
+                i -= 1
+                j -= 1
+        return s1_aligned, s2_aligned
+
+    with open(file, 'r') as f:
+        str1, str2 = f.read().splitlines()
+        n, m = len(str1), len(str2)
+
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    backtrack = [['' for _ in range(m+1)] for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        # Allow free rides only from the beginning of str1
+        dp[i][0] = 0
+        backtrack[i][0] = 's'
+    for j in range(1, m+1):
+        dp[0][j] = dp[0][j-1] - INDEL_PENALTY
+        backtrack[0][j] = 'r'
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            match_score = 1 if str1[i-1] == str2[j-1] else -1
+            dp[i][j] = max(dp[i-1][j]-INDEL_PENALTY, dp[i][j-1]-INDEL_PENALTY, dp[i-1][j-1]+match_score)
+
+            if dp[i][j] == dp[i-1][j]-INDEL_PENALTY:
+                backtrack[i][j] = 'd'
+            elif dp[i][j] == dp[i][j-1]-INDEL_PENALTY:
+                backtrack[i][j] = 'r'
+            elif dp[i][j] == dp[i-1][j-1]+match_score:
+                backtrack[i][j] = 'v'
+
+    x, y = n, m
+    max_val = 0
+    # Allow free rides only to the end of str1
+    for i in range(n+1):
+        if dp[i][m] > max_val:
+            max_val = dp[i][m]
+            x = i
+
+    return *backtracker(backtrack, str1, str2, x, y), dp[x][y]
+
+"""
+Code Challenge: Solve the Overlap Alignment Problem.
+
+Input: Two strings v and w, each of length at most 1000.
+Output: The score of an optimal overlap alignment of v and w, followed by an alignment of a suffix 
+    v' of v and a prefix w' of w achieving this maximum score. Use an alignment score in which 
+    matches count +1 and both the mismatch and indel penalties are 2.
+"""
+def overlap_alignment(file):
+    INDEL_PENALTY = 2
+
+    def backtracker(backtrack, s1, s2, n, m):
+        s1_aligned, s2_aligned = "", ""
+        i, j = n, m
+        while i != 0 or j != 0:
+            if backtrack[i][j] == 's':
+                break
+            elif backtrack[i][j] == 'd':
+                s1_aligned = s1[i-1] + s1_aligned
+                s2_aligned = '-' + s2_aligned
+                i -= 1
+            elif backtrack[i][j] == 'r':
+                s1_aligned = '-' + s1_aligned
+                s2_aligned = s2[j-1] + s2_aligned
+                j -= 1
+            else:
+                s1_aligned = s1[i-1] + s1_aligned
+                s2_aligned = s2[j-1] + s2_aligned
+                i -= 1
+                j -= 1
+        return s1_aligned, s2_aligned
+
+    with open(file, 'r') as f:
+        str1, str2 = f.read().splitlines()
+        n, m = len(str1), len(str2)
+
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    backtrack = [['' for _ in range(m+1)] for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        # Allow free rides only from the beginning of str1
+        dp[i][0] = 0
+        backtrack[i][0] = 's'
+    for j in range(1, m+1):
+        dp[0][j] = dp[0][j-1] - INDEL_PENALTY
+        backtrack[0][j] = 'r'
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            match_score = 1 if str1[i-1] == str2[j-1] else -2
+            dp[i][j] = max(dp[i-1][j]-INDEL_PENALTY, dp[i][j-1]-INDEL_PENALTY, dp[i-1][j-1]+match_score)
+
+            if dp[i][j] == dp[i-1][j]-INDEL_PENALTY:
+                backtrack[i][j] = 'd'
+            elif dp[i][j] == dp[i][j-1]-INDEL_PENALTY:
+                backtrack[i][j] = 'r'
+            elif dp[i][j] == dp[i-1][j-1]+match_score:
+                backtrack[i][j] = 'v'
+
+    x, y = n, m
+    max_val = 0
+    # Allow free rides only to the end of str2
+    for j in range(m+1):
+        if dp[n][j] > max_val:
+            max_val = dp[n][j]
+            y = j
+
+    return *backtracker(backtrack, str1, str2, x, y), dp[x][y]
+
+s1, s2, score = overlap_alignment('alignment.txt')
 print(score)
 print(s1)
 print(s2)
