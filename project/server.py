c2fa42618cdddcfe05eb03f48abfe6c4c07e5179
Alex Yu <yu.alexanderx@gmail.com>
2021-05-21 20:28:15 -0700

    Server logging

--- project/server.py
+++ project/server.py
@@ -34,6 +34,9 @@ class Server:
         self.client_locations = dict()
         self.client_at_log = dict()
 
+        logging.basicConfig(filename=f'server_{name}.log', format='%(levelname)s:%(message)s', filemode='w+', level=logging.INFO)
+        logging.info(f'Initialized server named {name}')
+
     def get_formatted_time_diff(self, start, end):
         time_diff = end - start
         time_diff_sign = '+' if time_diff > 0 else ''
@@ -55,16 +58,21 @@ class Server:
         fields = data.strip().split()
         
         if len(fields) == 0:
+            logging.error('No fields found in client command')
             writer.write(f'? {data}'.encode())
         elif fields[0] == 'IAMAT' and len(fields) == 4:
             # Handle IAMAT command from client
             client, coords, msg_time = fields[1:]
-            time_diff_str = self.get_formatted_time_diff(float(msg_time), curr_time)
+            msg_time = float(msg_time)
+            time_diff_str = self.get_formatted_time_diff(msg_time, curr_time)
 
+            logging.info(f'Received IAMAT command from client {client}')
             msg = f"AT {self.name} {time_diff_str} {client} {coords} {msg_time}\n"
             writer.write(msg.encode())
+
             self.client_at_log[client] = msg
             self.client_last_msg_time[client] = msg_time
+            logging.info(f'Sent IAMAT response to client {client}')
 
             await self.propogate_message(msg)
 
@@ -75,18 +83,23 @@ class Server:
             client, rad, limit = fields[1:]
             rad, limit = int(rad), int(limit)
             
+            logging.info(f'Received WHATSAT command from client {client}')
             if client not in self.client_locations.keys() or rad > 50 or limit > 20:
+                logging.error('Invalid parameter in WHATSAT command')
                 writer.write(f'? {data}'.encode())
             else:
                 response = await self.nearby_search_request(client, rad, limit)
+
                 writer.write(f"{self.client_at_log[client]}\n".encode())
                 writer.write(f"{json.dumps(response, indent=2)}\n".encode())
+                logging.info(f'Sent WHATSAT response to client {client}')
         elif fields[0] == "AT" and len(fields) == 6:
             # Handle propogated messages from other servers
             server, time_diff_str, client, coords, msg_time = fields[1:]
-            if msg_time not in self.client_last_msg_time.keys():
+            msg_time = float(msg_time)
+            if client not in self.client_last_msg_time.keys() or msg_time > self.client_last_msg_time[client]:
+                logging.info('Received new propogated message')
                 msg = f"AT {self.name} {time_diff_str} {client} {coords} {msg_time}\n"
-                writer.write(msg.encode())
                 self.client_at_log[client] = msg
                 self.client_last_msg_time[client] = msg_time
 
@@ -94,7 +107,10 @@ class Server:
 
                 split_idx = max(coords.rfind('+'), coords.rfind('-'))
                 self.client_locations[client] = (coords[:split_idx], coords[split_idx:])
+            else:
+                logging.info('Received old propogated message')
         else:
+            logging.error('Unknown command received')
             writer.write(f'? {data}'.encode())
 
         writer.close()
@@ -103,6 +119,7 @@ class Server:
         latitude, longitude = self.client_locations[client]
         location = f'{latitude},{longitude}'
 
+        logging.info('Making Nearby Places request to Google Places')
         url_req = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={API_KEY}&location={location}&radius={rad}'
         async with aiohttp.ClientSession(
             connector=aiohttp.TCPConnector(
@@ -116,6 +133,7 @@ class Server:
 
     async def propogate_message(self, msg):
         for other_server in SERVER_LINKS[self.name]:
+            logging.info(f'Propogating message to server {other_server}')
             _, writer = await asyncio.open_connection(self.host, PORT_MAPPING[other_server])
             writer.write(msg.encode())
             writer.close()
