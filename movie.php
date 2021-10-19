6f6978fa1c647ec9deff2b6600cc6fe7abc02eae
ColeTrammer <cole.trammer@gmail.com>
2021-10-19 15:09:08 -0700

    Finish implementing the core functionality for movie.php
    The average score and user reviews are simply fetched from the Review
    table.

--- movie.php
+++ movie.php
@@ -22,6 +22,17 @@
       echo '<h1>' . $title . ' ' . $year . ' ' . $rating . ' ' . $company . '</h1>';
       $statement->close();
 
+      // Compute the average score the movie recieved
+      $statement = $db->prepare("SELECT AVG(rating) FROM Review WHERE mid=?");
+      $statement->bind_param('i', $id);
+      $statement->execute();
+      $statement->bind_result($average_score);
+      $statement->fetch();
+
+      // If there are no ratings, than the MySQL query returns NULL
+      echo '<h3>Average Score: ' . ($average_score ? $average_score : 'no ratings') . '</h3>';
+      $statement->close();
+
       // Create table of all actors that were in the movie
       echo '<table><tr><th>Actors</th></tr>';
 
@@ -29,10 +40,26 @@
       $statement->bind_param('i', $id);
       $statement->execute();
       $statement->bind_result($aid, $first, $last);
+      $statement->fetch();
 
       while($statement->fetch()) {
-        echo '<tr><td><a href=/actor.php?id=' . $aid .'>' . $first . ' ' . $last . '</a></td></tr>';
+        echo '<tr><td><a href=actor.php?id=' . $aid .'>' . $first . ' ' . $last . '</a></td></tr>';
       }
+      $statement->close();
+
+      // Create a list of all user reviews
+      $statement = $db->prepare("SELECT name, rating, comment FROM Review WHERE mid=?");
+      $statement->bind_param('i', $id);
+      $statement->execute();
+      $statement->bind_result($user_name, $user_rating, $user_comment);
+
+      echo '<h3>User Reviews</h3><ul>';
+      while ($statement->fetch()) {
+        echo '<li><p>' . $user_name . ' gave a review of ' . $user_rating . ' / 5:</p><p>' . $user_comment . '</p>' . '</li>';
+      }
+      echo '</ul>';
+      echo '<a href="review.php?id=' . $id . '">Add Review</a>';
+
       $statement->close();
 
       echo '</table>';
