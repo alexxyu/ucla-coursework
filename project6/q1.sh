45861dae171e73646ff663bbb923d247a671c6fa
Alex Yu <alexy23@g.ucla.edu>
2021-11-20 18:12:32 -0800

    Update scripts to match spec

--- project6/q1.sh
+++ project6/q1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zcat < googlebooks-eng-all-1gram-20120701-s.gz | head -10000000 | awk '$3 >= 1000*$4' | cut -f 1,2
+zcat /home/cs143/data/googlebooks-eng-all-1gram-20120701-s.gz | awk '$3 >= 1000*$4' | cut -f 1,2
--- project6/q2.sh
+++ project6/q2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zcat < googlebooks-eng-all-1gram-20120701-s.gz | head -10000000 | awk '$4 >= 10000' | sort -k 2,2 | head -1 | cut -f 2
+zcat /home/cs143/data/googlebooks-eng-all-1gram-20120701-s.gz | awk '$4 >= 10000' | sort -k 2,2 | head -1 | cut -f 2
