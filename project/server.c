ae10a71ac0f29f25b69e152080ecd9a7c09ae4bb
Alex Yu <alexy23@g.ucla.edu>
2022-01-17 10:01:28 -0800

    Parse URL encoded spaces in filename

--- project/server.c
+++ project/server.c
@@ -5,11 +5,10 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
-int PORT = 8080;
-int BUF_SIZE = 1024;
-
-char* ERR404_HEADER = "HTTP/1.1 404 Not Found\r\n\r\n";
-char* ERR404_BODY = "<html><h1>Error 404: Not Found</h1></html>";
+const int PORT = 8080;
+const int BUF_SIZE = 1024;
+const char* ERR404_HEADER = "HTTP/1.1 404 Not Found\r\n\r\n";
+const char* ERR404_BODY = "<html><h1>Error 404: Not Found</h1></html>";
 
 void send_response(int socket, char* filename)
 {
@@ -17,7 +16,7 @@ void send_response(int socket, char* filename)
 
   if(file_fd == NULL) {
     // Resource does not exist
-    fprintf(stderr, "Resource %s not found\n", filename);
+    fprintf(stderr, "SERVER: Resource '%s' not found\n", filename);
     send(socket, ERR404_HEADER, strlen(ERR404_HEADER), 0);
     send(socket, ERR404_BODY, strlen(ERR404_BODY), 0);
   } else {
@@ -35,7 +34,7 @@ void send_response(int socket, char* filename)
       total_bytes += bytes_read;
     }
     
-    fprintf(stderr, "%d bytes sent\n", total_bytes);
+    fprintf(stderr, "SERVER: %d bytes sent for resource '%s'\n", total_bytes, filename);
     fclose(file_fd);
   }
 }
@@ -45,19 +44,31 @@ void process_request(int socket)
   char msg[BUF_SIZE];
   int bytes_read = recv(socket, msg, BUF_SIZE, 0);
   if(bytes_read < 0) {
-    fprintf(stderr, "Error while receiving message from socket: %s\n", strerror(errno));
+    fprintf(stderr, "SERVER: Error while receiving message from socket: %s\n", strerror(errno));
     return;
   }
   msg[bytes_read] = '\0';
-
-  fprintf(stderr, "Received following message from client:\n%s", msg);
+  fprintf(stderr, "SERVER: Received following message from client:\n%s", msg);
 
   char filename[BUF_SIZE];
   if(sscanf(msg, "GET %s HTTP/1.1\r\n", filename) != 1) {
     // HTTP request does not match correct format
-    fprintf(stderr, "Invalid GET request\n");
+    fprintf(stderr, "SERVER: Invalid GET request\n");
   } else {
-    send_response(socket, filename+1);
+    // Replace URL encoded spaces ('%20') with actual spaces
+    char filename_new[BUF_SIZE];
+    const char* delim = "%20";
+    char* token;
+
+    token = strtok(filename, delim);
+    strcat(filename_new, token);
+    while( (token = strtok(NULL, delim)) != NULL ) {
+      strcat(filename_new, " ");
+      strcat(filename_new, token);
+    } 
+
+    // Send response back to client (also, ignore first '/' in filename)
+    send_response(socket, filename_new+1);
   }
 }
 
@@ -73,35 +84,35 @@ int main(int argc, char *argv[])
 
   int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (sockfd < 0) {
-    fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
+    fprintf(stderr, "SERVER: Error creating socket: %s\n", strerror(errno));
     exit(1);
   }
 
   if( bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0 ) {
-    fprintf(stderr, "Error binding socket to address: %s\n", strerror(errno));
+    fprintf(stderr, "SERVER: Error binding socket to address: %s\n", strerror(errno));
     exit(1);
   }
 
   if( listen(sockfd, 1) < 0 ) {
-    fprintf(stderr, "Error while listening to socket: %s\n", strerror(errno));
+    fprintf(stderr, "SERVER: Error while listening to socket: %s\n", strerror(errno));
     exit(1);
   }
 
   while(1) {
     int new_sockfd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
     if( new_sockfd < 0 ) {
-      fprintf(stderr, "Error accepting client connection: %s\n", strerror(errno));
+      fprintf(stderr, "SERVER: Error accepting client connection: %s\n", strerror(errno));
       exit(1);
     }
 
     int child_pid;
     switch(child_pid = fork()) {
       case -1:
-        fprintf(stderr, "Error while forking process: %s\n", strerror(errno));
+        fprintf(stderr, "SERVER: Error while forking process: %s\n", strerror(errno));
         exit(1);
       case 0:
         // Child process: handle incoming HTTP request
-        fprintf(stderr, "Accepted connection from client\n");
+        fprintf(stderr, "SERVER: Accepted connection from client\n");
         process_request(new_sockfd);
         close(new_sockfd);
         exit(0);
