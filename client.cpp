f3023bee5747a7fde00fdd0a1b690ebabd600fb3
Alex Yu <alexy23@g.ucla.edu>
2022-02-16 13:34:08 -0800

    Basic server-client UDP

--- client.cpp
+++ client.cpp
@@ -1,8 +1,37 @@
-#include <string>
+#include <cstring>
 #include <thread>
 #include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 int main()
 {
-  std::cerr << "client is not implemented yet" << std::endl;
+  sockaddr_in addr;
+  socklen_t socklen;
+
+  int port = 5000;
+  char ip_str[65537] = "localhost";
+
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if(sock < 0) {
+    printf("Error creating socket: %s\n", strerror(errno));
+    return -1;
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  socklen = sizeof(addr);
+
+  char buf[65537] = "Hello, world!";
+  int ret = sendto(sock, buf, strlen(buf), 0, (sockaddr*)&addr, socklen);
+  if(ret < 0) {
+    printf("Error sending data: %s\n", strerror(errno));
+    return -1;
+  }
+  printf("SENT: %s\n", buf);
+
+  close(sock);
+  return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,43 @@
-#include <string>
+#include <cstring>
 #include <thread>
 #include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 int main()
 {
-  std::cerr << "server is not implemented yet" << std::endl;
+  sockaddr_in addr;
+  socklen_t socklen;
+
+  // TODO: port and ip_str
+  int port = 5000;
+  char ip_str[65537] = "localhost"; 
+
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if(sock < 0) {
+    printf("Error creating socket: %s\n", strerror(errno));
+    return -1;
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  socklen = sizeof(addr);
+
+  if( bind(sock, (sockaddr*)&addr, socklen) < 0 ) {
+    printf("Error binding to socket: %s\n", strerror(errno));
+    return -1;
+  }
+  
+  char buf[65537];
+  int ret = recvfrom(sock, buf, 65537, 0, (sockaddr*)&addr, &socklen);
+  buf[ret] = 0;
+  printf("RECEIVED: %s\n", buf);
+
+  inet_ntop(AF_INET, &addr.sin_addr, ip_str, socklen);
+  printf("Client IP address: %s port: %d\n", ip_str, ntohs(addr.sin_port));
+
+  close(sock);
+  return 0;
 }
