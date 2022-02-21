24e1166432e5dbde6a8a164b6366b3dbbdcbb453
ColeTrammer <cole.trammer@gmail.com>
2022-02-21 13:23:34 -0800

    autoformat the code
    We should remove the .clang_format file before submitting, since
    it is technically unnecessary.

--- client.cpp
+++ client.cpp
@@ -1,49 +1,48 @@
-#include <thread>
+#include <arpa/inet.h>
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
-{
-  if(argc != 4) {
-    std::cerr << "Usage: " << argv[0] << " <HOSTNAME-OR-IP> <PORT> <FILENAME>" << std::endl;
-    return 1;
-  }
-
-  sockaddr_in addr;
-  socklen_t socklen;
-
-  // TODO: port, ip_str, filename
-  char* ip_str = argv[1];
-  int port = atoi(argv[2]);
-  char* filename = argv[3];
-
-  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if(sock < 0) {
-    std::cerr << "ERROR: " << strerror(errno) << std::endl;
-    return 1;
-  }
-
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  socklen = sizeof(addr);
-
-  char buf[512];
-  std::ifstream ifs(filename);
-  while( !ifs.eof() ) {
-    ifs.read(buf, 512);
-    if( sendto(sock, buf, strlen(buf), 0, (sockaddr*)&addr, socklen) < 0) {
-      std::cerr << "ERROR: " << strerror(errno) << std::endl;
-      return 1;
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <HOSTNAME-OR-IP> <PORT> <FILENAME>" << std::endl;
+        return 1;
     }
-    memset(buf, 0, 512);
-  }
-  ifs.close();
 
-  close(sock);
-  return 0;
+    sockaddr_in addr;
+    socklen_t socklen;
+
+    // TODO: port, ip_str, filename
+    char* ip_str = argv[1];
+    int port = atoi(argv[2]);
+    char* filename = argv[3];
+
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        std::cerr << "ERROR: " << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    socklen = sizeof(addr);
+
+    char buf[512];
+    std::ifstream ifs(filename);
+    while (!ifs.eof()) {
+        ifs.read(buf, 512);
+        if (sendto(sock, buf, strlen(buf), 0, (sockaddr*) &addr, socklen) < 0) {
+            std::cerr << "ERROR: " << strerror(errno) << std::endl;
+            return 1;
+        }
+        memset(buf, 0, 512);
+    }
+    ifs.close();
+
+    close(sock);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,57 +1,57 @@
-#include <thread>
+#include <arpa/inet.h>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
-{
-  if(argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <PORT> <FILE-DIR>" << std::endl;
-    return 1;
-  }
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <PORT> <FILE-DIR>" << std::endl;
+        return 1;
+    }
 
-  sockaddr_in addr;
-  socklen_t socklen;
+    sockaddr_in addr;
+    socklen_t socklen;
 
-  int port = atoi(argv[1]);
-  char* filedir = argv[2];
+    int port = atoi(argv[1]);
+    char* filedir = argv[2];
 
-  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if(sock < 0) {
-    std::cerr << "ERROR: " << strerror(errno) << std::endl;
-    return 1;
-  }
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        std::cerr << "ERROR: " << strerror(errno) << std::endl;
+        return 1;
+    }
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  socklen = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    socklen = sizeof(addr);
 
-  if( bind(sock, (sockaddr*)&addr, socklen) < 0 ) {
-    std::cerr << "ERROR: " << strerror(errno) << std::endl;
-    return 1;
-  }
+    if (bind(sock, (sockaddr*) &addr, socklen) < 0) {
+        std::cerr << "ERROR: " << strerror(errno) << std::endl;
+        return 1;
+    }
 
-  std::stringstream filepath;
-  filepath << filedir << "/" << "1.file";
-  std::ofstream ofs(filepath.str());
+    std::stringstream filepath;
+    filepath << filedir << "/"
+             << "1.file";
+    std::ofstream ofs(filepath.str());
 
-  int bytes;
-  char buf[512];
-  while( (bytes = recvfrom(sock, buf, 512, MSG_WAITALL, (sockaddr*)&addr, &socklen)) >= 512 ) {
+    int bytes;
+    char buf[512];
+    while ((bytes = recvfrom(sock, buf, 512, MSG_WAITALL, (sockaddr*) &addr, &socklen)) >= 512) {
+        ofs.write(buf, bytes);
+    }
     ofs.write(buf, bytes);
-  }
-  ofs.write(buf, bytes);
-  ofs.close();
-  
-  char client_ip[256];
-  inet_ntop(AF_INET, &addr.sin_addr, client_ip, socklen);
-  std::cerr << "Client IP address: " << client_ip << " port: " << ntohs(addr.sin_port) << std::endl;
+    ofs.close();
 
-  close(sock);
-  return 0;
+    char client_ip[256];
+    inet_ntop(AF_INET, &addr.sin_addr, client_ip, socklen);
+    std::cerr << "Client IP address: " << client_ip << " port: " << ntohs(addr.sin_port) << std::endl;
+
+    close(sock);
+    return 0;
 }
