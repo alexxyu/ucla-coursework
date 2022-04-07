5f3d6e25dde7455cbeeb70c272bead31296606d1
Alex Yu <alexy23@g.ucla.edu>
2022-04-07 09:18:40 -0700

    lab1: First attempt at parallel gemm

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -1,10 +1,23 @@
 // Header inclusions, if any...
 
+#include <cstring>
+#include <omp.h>
 #include "lib/gemm.h"
 
 // Using declarations, if any...
 
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
-  // Your code goes here...
+  int i, k, j;
+  
+  #pragma omp parallel for private(j,k) schedule(dynamic, 128)
+  for (i=0; i<kI; i++) {
+    std::memset(c[i], 0, sizeof(float) * kJ);
+    for (k=0; k<kK; k++) {
+      float p = a[i][k];
+      for (j=0; j<kJ; j++) {
+        c[i][j] += p * b[k][j];
+      }
+    }
+  }
 }
