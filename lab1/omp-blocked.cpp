d4a7aec5478f31ea75dd9f8686200fc6622d15ac
Alex Yu <alexy23@g.ucla.edu>
2022-04-08 08:20:02 -0700

    lab1: Reduce number of redundant calculations

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -6,7 +6,7 @@
 #include "lib/gemm.h"
 
 // Using declarations, if any...
-#define BLOCK_SIZE 64
+#define BLOCK_SIZE 256
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
@@ -14,32 +14,31 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   int i, k, j;
   int ii, jj, kk;
 
-  float aT[BLOCK_SIZE][kK];
-  float bT[kK][BLOCK_SIZE];
+  float aT[BLOCK_SIZE][BLOCK_SIZE];
+  float bT[BLOCK_SIZE][BLOCK_SIZE];
   
-  #pragma omp parallel for shared(c)
-  for (i=0; i<kI; i++) {
-    std::memset(c[i], 0, sizeof(float) * kI);
-  }
-
   for (i=0; i<kI; i+=BLOCK_SIZE) {
-    #pragma omp parallel for shared(a,aT)
-    for (ii=0; ii<BLOCK_SIZE; ii++) {
-      std::memcpy(aT[ii], a[i+ii], sizeof(float) * kK);
-    }
+    std::memset(c[i], 0, sizeof(float) * kI);
 
-    for (j=0; j<kJ; j+=BLOCK_SIZE) {
-      #pragma omp parallel for shared(b,bT)
-      for (kk=0; kk<kK; kk++) {
-        std::memcpy(bT[kk], b[kk]+j, sizeof(float) * BLOCK_SIZE);
+    for (k=0; k<kK; k+=BLOCK_SIZE) {
+      #pragma omp parallel for shared(a,aT)
+      for (ii=0; ii<BLOCK_SIZE; ii++) {
+        std::memcpy(aT[ii], a[i+ii]+k, sizeof(float) * BLOCK_SIZE);
       }
 
-      #pragma omp parallel for shared(aT,bT) private(p,kk,jj)
-      for (ii=0; ii<BLOCK_SIZE; ii++) {
-        for (kk=0; kk<kK; kk++) {
-          float p = aT[ii][kk];
-          for (jj=0; jj<BLOCK_SIZE; jj++) {
-            c[i+ii][j+jj] += p * bT[kk][jj];
+      for (j=0; j<kJ; j+=BLOCK_SIZE) {
+        #pragma omp parallel for shared(b,bT)
+        for (kk=0; kk<BLOCK_SIZE; kk++) {
+          std::memcpy(bT[kk], b[k+kk]+j, sizeof(float) * BLOCK_SIZE);
+        }
+
+        #pragma omp parallel for shared(aT,bT) private(p,kk,jj)
+        for (ii=0; ii<BLOCK_SIZE; ii++) {
+          for (kk=0; kk<BLOCK_SIZE; kk++) {
+            p = aT[ii][kk];
+            for (jj=0; jj<BLOCK_SIZE; jj++) {
+              c[i+ii][j+jj] += p * bT[kk][jj];
+            }
           }
         }
       }
