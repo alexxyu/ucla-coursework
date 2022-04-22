bd60257c88b03c37af01cde819c32347aa9e3c66
Alex Yu <alexy23@g.ucla.edu>
2022-04-22 10:09:36 -0700

    lab1: Optimize blocked omp code one more time

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -21,14 +21,13 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   float aT[BLOCK_SIZE][BLOCK_SIZE];
   float bT[BLOCK_SIZE][BLOCK_SIZE];
   
+  #pragma omp parallel for shared(a,b,c) private(k,j,ii,jj,kk,aT,bT,p)
   for (i=0; i<kI; i+=BLOCK_SIZE) {
     for (k=0; k<kK; k+=BLOCK_SIZE) {
-      #pragma omp parallel for shared(i,k,a,aT)
       for (ii=0; ii<BLOCK_SIZE; ii++) {
         std::memcpy(aT[ii], a[i+ii]+k, sizeof(float) * BLOCK_SIZE);
       }
 
-      #pragma omp parallel for shared(i,k,aT,b,c) private(ii,jj,kk,bT,p)
       for (j=0; j<kJ; j+=BLOCK_SIZE) {
         for (kk=0; kk<BLOCK_SIZE; kk++) {
           std::memcpy(bT[kk], b[k+kk]+j, sizeof(float) * BLOCK_SIZE);
