bdf5d67e41b347189eb20d22475326d3c502607f
Alex Yu <alexy23@g.ucla.edu>
2022-04-26 19:57:28 -0700

    lab2: Use different block sizes for each loop

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -9,29 +9,31 @@
 // You can directly use aligned_alloc
 // with lab2::aligned_alloc(...)
 
-#define BLOCK_LEN 16
-#define BLOCK_SIZE BLOCK_LEN*BLOCK_LEN
+#define BLOCK_LEN_I 64
+#define BLOCK_LEN_J 1024
+#define BLOCK_LEN_K 8
 #define ALIGNMENT_SIZE 4096
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
-  int i, j, k, r, p;
+  int i, j, k;
   int ii, jj, kk;
-  int pnum, pid, runs_I;
+  int pnum, pid, nRows;
 
   MPI_Comm_size(MPI_COMM_WORLD, &pnum);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-  runs_I = kI / pnum / BLOCK_LEN;
+  nRows = kI / pnum;
 
   // aBlocks is used to blocks of A assigned to this processor (row-wise)
-  float* aBlocks = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * runs_I * BLOCK_LEN * kK);
-  float* aBlock = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * BLOCK_SIZE);
+  float* aBlocks = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * nRows * kK);
+  float* aBlock = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * BLOCK_LEN_I * BLOCK_LEN_K);
+
   float* bCopy = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * kK * kJ);
 
-  // cBuffer is used for storing computations for C, to be gathered at the end of each run
-  float* cBuffer = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * runs_I * BLOCK_LEN * kJ);
-  std::memset(cBuffer, 0, sizeof(float) * runs_I * BLOCK_LEN * kJ);
+  // cBuffer is used for storing this processor's computations for assigned rows in C
+  float* cBuffer = (float*) lab2::aligned_alloc(ALIGNMENT_SIZE, sizeof(float) * nRows * kJ);
+  std::memset(cBuffer, 0, sizeof(float) * nRows * kJ);
 
   if (pid == 0) {
     std::memcpy(bCopy, b, sizeof(float) * kK * kJ);
@@ -43,27 +45,23 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   );
 
   MPI_Scatter(
-    a, runs_I * BLOCK_LEN * kK, MPI_FLOAT,
-    aBlocks, runs_I * BLOCK_LEN * kK, MPI_FLOAT,
+    a, kI / pnum * kK, MPI_FLOAT,
+    aBlocks, kI / pnum * kK, MPI_FLOAT,
     0, MPI_COMM_WORLD
   );
 
-  // Iterate over each run, where each processor works on BLOCK_LEN rows in C
-  for (r=0; r<runs_I; r++) {
-    i = r * BLOCK_LEN;
-
-    for (k=0; k<kK; k+=BLOCK_LEN) {
-      for (ii=0; ii<BLOCK_LEN; ii++) {
-        std::memcpy(&aBlock[ii*BLOCK_LEN], &aBlocks[(r*BLOCK_LEN+ii)*kK + k], sizeof(float) * BLOCK_LEN);
+  for (i=0; i<nRows; i+=BLOCK_LEN_I) {
+    for (k=0; k<kK; k+=BLOCK_LEN_K) {
+      for (ii=0; ii<BLOCK_LEN_I; ii++) {
+        std::memcpy(&aBlock[ii*BLOCK_LEN_K], &aBlocks[(i+ii)*kK + k], sizeof(float) * BLOCK_LEN_K);
       }
 
-      for (j=0; j<kJ; j+=BLOCK_LEN) {
-        // Perform matrix multiplication on individual blocks
-        for (ii=0; ii<BLOCK_LEN; ii++) {
-          for (jj=0; jj<BLOCK_LEN; jj++) {
+      for (j=0; j<kJ; j+=BLOCK_LEN_J) {
+        for (ii=0; ii<BLOCK_LEN_I; ii++) {
+          for (jj=0; jj<BLOCK_LEN_J; jj++) {
             float cVal = 0;
-            for (kk=0; kk<BLOCK_LEN; kk++) {
-              cVal += aBlock[ii*BLOCK_LEN + kk] * bCopy[(k+kk)*kJ + j+jj];
+            for (kk=0; kk<BLOCK_LEN_K; kk++) {
+              cVal += aBlock[ii*BLOCK_LEN_K + kk] * bCopy[(k+kk)*kJ + j+jj];
             }
             cBuffer[(i+ii)*kJ + (j+jj)] += cVal;
           }
@@ -72,10 +70,10 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
     }
   }
 
-  // Gather computed rows from this run across all processors into C
+  // Gather computed rows across all processors into C
   MPI_Gather(
-    cBuffer, runs_I * BLOCK_LEN * kJ, MPI_FLOAT,
-    c, runs_I * BLOCK_LEN * kJ, MPI_FLOAT,
+    cBuffer, nRows * kJ, MPI_FLOAT,
+    c, nRows * kJ, MPI_FLOAT,
     0, MPI_COMM_WORLD
   );
 
