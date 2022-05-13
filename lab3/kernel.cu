ccc72f5595f63eb228a064a73da1df4f27c4f577
Alex Yu <alexy23@g.ucla.edu>
2022-05-12 17:03:28 -0700

    lab3: Refactor

--- lab3/kernel.cu
+++ lab3/kernel.cu
@@ -2,7 +2,7 @@
 #include "kernel.h"
 
 #define TILE_WIDTH 32
-#define BLOCK_J 8
+#define STEP_J 8
 
 __global__ void cnn_gpu(float* input,
     float* weight,
@@ -12,44 +12,44 @@ __global__ void cnn_gpu(float* input,
   // OUtput size: 256 x 112 x 112
   // GPU specs: 16 SMs, 32 blocks/SM, 2048 threads/SM (32K threads total)
 
-  const int ch = kNum / gridDim.x;
-  const int bi = blockIdx.x * ch;
+  const int NROWS = kNum / gridDim.x;
+  const int ROW = blockIdx.x * NROWS;
   const int tr = threadIdx.y;
   const int tc = threadIdx.x;
 
   __shared__ float C[TILE_WIDTH][TILE_WIDTH];
-  __shared__ float weightShared[BLOCK_J][kKernel][kKernel];
-  __shared__ float inputShared[BLOCK_J][TILE_WIDTH+kKernel-1][TILE_WIDTH+kKernel-1];
+  __shared__ float weightShared[STEP_J][kKernel][kKernel];
+  __shared__ float inputShared[STEP_J][TILE_WIDTH+kKernel-1][TILE_WIDTH+kKernel-1];
   
-  for (int i = bi; i < bi+ch; i++) {
+  for (int i = ROW; i < ROW+NROWS; i++) {
     for (int h = tr; h < kImSize; h += TILE_WIDTH) {
       for (int w = tc; w < kImSize; w += TILE_WIDTH) {
         // Bias
         float reg = bias[i];
 
         // Convolution
-        for (int bj = 0; bj < kNum; bj += BLOCK_J) {
-          for (int j = 0; j < BLOCK_J; j++) {
-            inputShared[j][tr][tc] = input(bj+j, h, w);
+        for (int j = 0; j < kNum; j += STEP_J) {
+          for (int jj = 0; jj < STEP_J; jj++) {
+            inputShared[jj][tr][tc] = input(j+jj, h, w);
             if (tc < kKernel-1) {
-              inputShared[j][tr][tc+TILE_WIDTH] = input(bj+j, h, w+TILE_WIDTH);
+              inputShared[jj][tr][tc+TILE_WIDTH] = input(j+jj, h, w+TILE_WIDTH);
             }
             if (tr < kKernel-1) {
-              inputShared[j][tr+TILE_WIDTH][tc] = input(bj+j, h+TILE_WIDTH, w);
+              inputShared[jj][tr+TILE_WIDTH][tc] = input(j+jj, h+TILE_WIDTH, w);
             }
             if (tr < kKernel-1 && tc < kKernel-1) {
-              inputShared[j][tr+TILE_WIDTH][tc+TILE_WIDTH] = input(bj+j, h+TILE_WIDTH, w+TILE_WIDTH);
+              inputShared[jj][tr+TILE_WIDTH][tc+TILE_WIDTH] = input(j+jj, h+TILE_WIDTH, w+TILE_WIDTH);
             }
             if (tr < kKernel && tc < kKernel) {
-              weightShared[j][tr][tc] = weight(i, bj+j, tr, tc);
+              weightShared[jj][tr][tc] = weight(i, j+jj, tr, tc);
             }
           }
           __syncthreads();
 
-          for (int j = 0; j < BLOCK_J; j++) {
+          for (int jj = 0; jj < STEP_J; jj++) {
             for (int p = 0; p < kKernel; ++p) {
               for (int q = 0; q < kKernel; ++q) {
-                reg += weightShared[j][p][q] * inputShared[j][tr + p][tc + q];
+                reg += weightShared[jj][p][q] * inputShared[jj][tr + p][tc + q];
               }
             }
           }
